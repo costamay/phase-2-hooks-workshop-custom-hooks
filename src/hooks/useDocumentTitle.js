9592f8b3a72bcbe89d9b902d3cf59faffedf8ef0
costamay <titusouko@gmail.com>
2022-05-10 11:41:48 +0300

    Completed assigment

---
+++ src/hooks/useDocumentTitle.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+
+function useDocumentTitle(){
+
+        useEffect(() => {
+            document.title = "Welcome to the home page!";
+          }, []);
+    
+}
+
+export default useDocumentTitle;
